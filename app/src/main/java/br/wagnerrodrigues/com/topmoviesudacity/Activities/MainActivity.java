1dfd78b0115941c41bd697e81d2f772c41ccb56d
Wagner Rodrigues <wagnersr92@gmail.com>
2019-01-03 21:05:19 -0200

    Initial commit

---
+++ app/src/main/java/br/wagnerrodrigues/com/topmoviesudacity/Activities/MainActivity.java
@@ -0,0 +1,15 @@
+package br.wagnerrodrigues.com.topmoviesudacity.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import br.wagnerrodrigues.com.topmoviesudacity.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
